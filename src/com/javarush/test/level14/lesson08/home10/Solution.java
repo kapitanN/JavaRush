df8b7a1e21e15ab8b1d97be328ca7df492b9dc85
Nikita Shapovalov <nikita.shapovalov.94@gmail.com>
2015-05-04 18:01:13 +0300

    from IDE

--- src/com/javarush/test/level14/lesson08/home10/Solution.java
+++ src/com/javarush/test/level14/lesson08/home10/Solution.java
@@ -29,40 +29,51 @@ public class Solution
 {
     public static void main(String[] args)
     {
+
         new NotIncapsulatedClass();
     }
 
     public static class NotIncapsulatedClass
     {
-        public NotIncapsulatedClass()
+        private List<Number> initList (List<Number> list)
         {
-            List<Number> list = new LinkedList<Number>();
-            //1
+
             list.add(new Double(1000f));
             list.add(new Double("123e-445632"));
             list.add(new Float(-90 / -3));
             list.remove(new Double("123e-445632"));
+            return list;
+        }
 
-            //2 - Исправь 2 ошибки
-            for (int i = 0; i <= list.size(); i--)
+        private void printListValues(List<Number> list)
+        {
+            for (int i = 0; i < list.size(); i++)
             {
                 System.out.println(list.get(i));
             }
+        }
 
-            //3
+        private void processCastedObjects(List<Number> list)
+        {
             for (Number object : list)
             {
                 //Исправь 2 ошибки
-                if (object instanceof Float)
+                if (object instanceof Double)
                 {
                     Double a = (Double) object;
                     System.out.println("Is float value defined? " + a.isNaN());
-                } else if (object instanceof Double)
+                } else if (object instanceof Float)
                 {
                     Float a = (Float) object;
                     System.out.println("Is double value infinite? " + a.isInfinite());
                 }
             }
         }
+        public NotIncapsulatedClass(){
+            List<Number> list = new LinkedList<Number>();
+
+        }
     }
 }
+
+
