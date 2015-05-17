edcef9016ff789b6081e11eb75e3c4218cc23548
Nikita Shapovalov <nikita.shapovalov.94@gmail.com>
2015-05-17 21:40:07 +0300

    from IDE

---
+++ src/com/javarush/test/level15/lesson12/home05/SubSolution.java
@@ -0,0 +1,62 @@
+package com.javarush.test.level15.lesson12.home05;
+
+/**
+ * Created by Nikita on 14.05.2015.
+ */
+public class SubSolution extends Solution
+{
+    public SubSolution(Object const1)
+    {
+        super(const1);
+    }
+
+    public SubSolution(Integer const2)
+    {
+        super(const2);
+    }
+
+    public SubSolution(String const3)
+    {
+        super(const3);
+    }
+
+    protected SubSolution(Number const4)
+    {
+        super(const4);
+    }
+
+    protected SubSolution(Character const2)
+    {
+        super(const2);
+    }
+
+    protected SubSolution(Double const3)
+    {
+        super(const3);
+    }
+
+    SubSolution(Byte const1)
+    {
+        super(const1);
+    }
+
+    SubSolution(Exception const2)
+    {
+        super(const2);
+    }
+
+    SubSolution(float const3)
+    {
+        super(const3);
+    }
+
+    private SubSolution(boolean const1){
+        super(const1);
+    }
+    private SubSolution(Short const2){
+        super(const2);
+    }
+    private SubSolution(Float const3){
+        super(const3);
+    }
+}
