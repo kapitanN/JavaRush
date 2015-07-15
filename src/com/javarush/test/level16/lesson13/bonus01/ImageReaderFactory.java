0136630e9ce791e873d38403f0d6019383381295
Nikita Shapovalov <nikita.shapovalov.94@gmail.com>
2015-07-15 17:00:12 +0300

    new

--- src/com/javarush/test/level16/lesson13/bonus01/ImageReaderFactory.java
+++ src/com/javarush/test/level16/lesson13/bonus01/ImageReaderFactory.java
@@ -1,8 +1,28 @@
 package com.javarush.test.level16.lesson13.bonus01;
 
+import com.javarush.test.level16.lesson13.bonus01.common.*;
+
 /**
  * Created by Nikita on 15.07.2015.
  */
 public class ImageReaderFactory
 {
+    public static ImageReader getReader(ImageTypes types)  {
+
+        ImageReader reader;
+            if (types == ImageTypes.BMP){
+                reader = new BmpReader();
+            }
+            else if (types == ImageTypes.JPG){
+                reader = new JpgReader();
+            }
+            else if (types == ImageTypes.PNG){
+                reader = new PngReader();
+            }
+
+        else {
+                throw new IllegalArgumentException("Неизвестный тип картинки");
+            }
+          return reader;
+    }
 }
