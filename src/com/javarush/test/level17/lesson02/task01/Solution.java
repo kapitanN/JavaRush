adbfb117d00f3e05910126da9a486f02e6fb1002
Nikita Shapovalov <nikita.shapovalov.94@gmail.com>
2015-07-20 23:48:56 +0300

    new

--- src/com/javarush/test/level17/lesson02/task01/Solution.java
+++ src/com/javarush/test/level17/lesson02/task01/Solution.java
@@ -29,7 +29,7 @@ public class Solution {
         public static void removeNote(String threadName) {
             String note = notes.remove(0);
             if (note == null) {
-                System.out.println("Другая нить удалила нашу заметку");
+                System.out.println("Другая нить удалила нашу заметку ");
             } else if (!note.startsWith(threadName)) {
                 System.out.println("Нить [" + threadName + "] удалила чужую заметку [" + note + "]");
             }
