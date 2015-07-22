40bc272d1b1afc6140a791784ef0d432aa6c94c2
nikita <nikita.shapovalov.94@gmail.com>
2015-07-23 02:14:04 +0300

    from linux

--- src/com/javarush/test/level08/lesson08/task05/Solution.java
+++ src/com/javarush/test/level08/lesson08/task05/Solution.java
@@ -12,12 +12,27 @@ public class Solution
 {
     public static HashMap<String, String> createMap()
     {
+        HashMap<String, String> map = new HashMap<String,String>();
+        map.put("Фамилия","Имя");
+        map.put("Фамилия1","Имя1");
+        map.put("Фамилия2","Имя2");
+        map.put("Фамилия3","Имя3");
+        map.put("Фамилия4","Имя4");
+        map.put("Фамилия5","Имя5");
+        map.put("Фамилия6","Имя6");
+        map.put("Фамилия7","Имя7");
+        map.put("Фамилия8","Имя8");
+        map.put("Фамилия9","Имя9");
+        return map;
         //Напишите тут ваш код
 
     }
 
     public static void removeTheFirstNameDuplicates(HashMap<String, String> map)
     {
+        for (String value : createMap().values()){
+            //if()
+        }
         //Напишите тут ваш код
 
     }
@@ -30,5 +45,9 @@ public class Solution
             if (pair.getValue().equals(value))
                 map.remove(pair.getKey());
         }
+    }
+
+    public static void main(String[] args){
+        Solution.removeTheFirstNameDuplicates(Solution.createMap());
     }
 }
