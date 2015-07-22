11fa182851fd9eece57b6f95d7b1db176dde39da
nikita <nikita.shapovalov.94@gmail.com>
2015-07-23 01:31:27 +0300

    from linux

--- src/com/javarush/test/level17/lesson04/task04/Solution.java
+++ src/com/javarush/test/level17/lesson04/task04/Solution.java
@@ -9,9 +9,16 @@ package com.javarush.test.level17.lesson04.task04;
 public class Solution {
     public static class OurPresident {
 
-        private static OurPresident president;
+        static{
+            synchronized(Solution.class){
+                OurPresident president = new OurPresident();
 
+            }
+        }
+
+        private static OurPresident president;
         private OurPresident() {
+
         }
 
         public static OurPresident getOurPresident() {
