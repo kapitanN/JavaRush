efaf3b8ed79491451e1529da6010b9bb1258a35e
nikita <nikita.shapovalov.94@gmail.com>
2015-07-23 20:00:22 +0300

    from linux

--- src/com/javarush/test/level08/lesson08/task04/Solution.java
+++ src/com/javarush/test/level08/lesson08/task04/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level08.lesson08.task04;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 /* Удалить всех людей, родившихся летом
 Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: «фамилия» - «дата рождения».
@@ -14,14 +15,36 @@ public class Solution
     {
         HashMap<String, Date> map = new HashMap<String, Date>();
         map.put("Сталлоне", new Date("JUNE 1 1980"));
-
+        map.put("Сталлоне1", new Date("JUNE 2 1980"));
+        map.put("Сталлоне2", new Date("JUNE 3 1980"));
+        map.put("Сталлоне3", new Date("JUNE 4 1980"));
+        map.put("Сталлоне4", new Date("SEPTEMBER 5 1980"));
+        map.put("Сталлоне5", new Date("OCTOBER 5 1980"));
+        map.put("Сталлоне6", new Date("JUNE 1 1980"));
+        map.put("Сталлоне7", new Date("JUNE 1 1980"));
+        map.put("Сталлоне8", new Date("JUNE 1 1980"));
+        map.put("Сталлоне9", new Date("JUNE 1 1980"));
+        return map;
         //Напишите тут ваш код
 
     }
 
     public static void removeAllSummerPeople(HashMap<String, Date> map)
     {
+        HashMap<String, Date> map1 = new HashMap<String, Date>(map);
+        for (Map.Entry<String, Date> pair : map1.entrySet()){
+            if(pair.getValue().getMonth()>4 & pair.getValue().getMonth()<8){
+                map.remove(pair.getKey());
+            }
+        }
+
         //Напишите тут ваш код
 
+    }
+
+    public static void  main(String[] args){
+        HashMap<String, Date> map = createMap();
+        removeAllSummerPeople(map);
+        System.out.println(map);
     }
 }
--- src/com/javarush/test/level08/lesson08/task05/Solution.java
+++ src/com/javarush/test/level08/lesson08/task05/Solution.java
@@ -13,16 +13,16 @@ public class Solution
     public static HashMap<String, String> createMap()
     {
         HashMap<String, String> map = new HashMap<String,String>();
-        map.put("Фамилия","Имя");
-        map.put("Фамилия1","Имя1");
-        map.put("Фамилия2","Имя2");
-        map.put("Фамилия3","Имя3");
-        map.put("Фамилия4","Имя4");
-        map.put("Фамилия5","Имя5");
-        map.put("Фамилия6","Имя6");
-        map.put("Фамилия7","Имя7");
-        map.put("Фамилия8","Имя8");
-        map.put("Фамилия9","Имя9");
+        map.put("Фамилия", "Имя");
+        map.put("Фамилия1", "Имя");
+        map.put("Фамилия2", "Имя2");
+        map.put("Фамилия3", "Имя3");
+        map.put("Фамилия4", "Имя4");
+        map.put("Фамилия5", "Имя5");
+        map.put("Фамилия6", "Имя6");
+        map.put("Фамилия7", "Имя7");
+        map.put("Фамилия8", "Имя8");
+        map.put("Фамилия9", "Имя9");
         return map;
         //Напишите тут ваш код
 
@@ -30,9 +30,22 @@ public class Solution
 
     public static void removeTheFirstNameDuplicates(HashMap<String, String> map)
     {
-        for (String value : createMap().values()){
-            //if()
+        HashMap<String, String> map2 = new HashMap<String, String>(map);
+        HashMap<String, String> map3 = new HashMap<String, String>(map);
+
+        //цикл по map2
+        for (Map.Entry<String, String> pair : map2.entrySet())
+        {
+            //удаляем из map3 пару, которая соответствует pair
+            map3.remove(pair.getKey());
+            //проверяем на наличе  в map3 значений соответсвующего значению из pair
+            if (map3.containsValue(pair.getValue()))
+            {   //Если есть такие же значения - вызываем удаления текующего значения pair из переданной map
+                removeItemFromMapByValue(map, pair.getValue());
+            }
         }
+
+        System.out.println(map);
         //Напишите тут ваш код
 
     }
