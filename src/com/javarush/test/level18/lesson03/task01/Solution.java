fe0adec1f777489302a90b5006cc4ce5e63f8bdc
nikita <nikita.shapovalov.94@gmail.com>
2015-07-27 23:52:17 +0300

    from linux

---
+++ src/com/javarush/test/level18/lesson03/task01/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level18.lesson03.task01;
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* Максимальный байт
+Ввести с консоли имя файла
+Найти максимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        FileInputStream input = new FileInputStream(name);
+        byte [] buffer = new byte[100];
+        int tmp;
+        int max = 0;
+        while(input.available()>0){
+            tmp = input.read();
+            if (tmp>max){
+                max = tmp;
+            }
+
+        }
+        System.out.println(max);
+        reader.close();
+        input.close();
+
+    }
+}
