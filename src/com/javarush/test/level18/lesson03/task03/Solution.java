e421727742f0fe12bbffaf943a3f2f2cbef91722
nikita <nikita.shapovalov.94@gmail.com>
2015-07-30 15:54:26 +0300

    from linux

--- src/com/javarush/test/level18/lesson03/task03/Solution.java
+++ src/com/javarush/test/level18/lesson03/task03/Solution.java
@@ -19,26 +19,51 @@ public class Solution {
         FileInputStream input = new FileInputStream(name);
         ArrayList<Integer> data = new ArrayList<Integer>();
         ArrayList<Integer> oftenData = new ArrayList<Integer>();
-        HashMap<Integer, Integer> data1 = new HashMap<Integer, Integer>();
-        int count = 0;
-        byte [] buffer = new byte[1000];
+        String res = "";
+       //HashMap<Integer, Integer> data1 = new HashMap<Integer, Integer>();
+
         while(input.available()>0){
             data.add(input.read());
         }
 
+        int [] count = new int[data.size()];
+        for (int i = 0; i < data.size(); i++)
+        {
+            count[i] = 0;
+            for (int j = 0; j < data.size(); j++)
+            {
+                if(data.get(i) == data.get(j)) count[i]++;
+            }
 
-        Solution.oftenBytes(data,data1);
+        }
+
+
+        int max = Integer.MIN_VALUE;
+        for (int it : count)
+        {
+            if (it>max) max = it;
+        }
+
+        for (int i = 0; i<data.size(); i++)
+        {
+            if (!oftenData.contains(data.get(i))&&count[i]==max) oftenData.add(data.get(i));
+        }
+        for (int y: oftenData)
+        {
+            res = y + " ";
+        }
+        System.out.println(res);
+
+        reader.close();
+        input.close();
 
 
 }
-    public static void oftenBytes(ArrayList<Integer> data, HashMap<Integer, Integer> data1){
+    /*public static void oftenBytes(ArrayList<Integer> data, HashMap<Integer, Integer> data1){
 
         int value = 0;
         for (int i = 0; i < data.size(); i++)
         {
-
-
-            System.out.println(data1.containsKey(data.get(i)));
             if (data1.containsKey(data.get(i))){
                 data1.put(data.get(i),value++);
             }
@@ -46,27 +71,19 @@ public class Solution {
                 data1.put(data.get(i), value );
             }
         }
-
-
-        for (int i = 0; i < data.size(); i++)
-        {
-            System.out.println(data.get(i));
-        }
-
-        for (Integer dataValue : data1.values()){
-            System.out.println(dataValue);
-        }
     }
 
     public static void sort(HashMap<Integer, Integer> data){
         ArrayList sortedList = new ArrayList(data.entrySet());
-        Collections.sort(sortedList, new Comparator(){
-
+        Collections.sort(sortedList, new Comparator<Map.Entry<Integer, Integer>>()
+        {
             @Override
-            public int compare(Map.Entry e1, Map.Entry e2){
+            public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2)
+            {
                 return e1.getValue().compareTo(e2.getValue());
             }
-
         });
-    }
+
+            System.out.println(sortedList.get(sortedList.size()-1));
+    }*/
 }
--- src/com/javarush/test/level18/lesson03/task05/Solution.java
+++ src/com/javarush/test/level18/lesson03/task05/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.test.level18.lesson03.task05;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
 
 /* Сортировка байт
 Ввести с консоли имя файла
@@ -18,5 +21,51 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        FileInputStream input = new FileInputStream(name);
+        ArrayList<Integer> data = new ArrayList<Integer>();
+        ArrayList<Integer> oftenData = new ArrayList<Integer>();
+        String res = "";
+        HashMap<Integer, Integer> data1 = new HashMap<Integer, Integer>();
+
+        while(input.available()>0){
+            data.add(input.read());
+        }
+
+        Solution.sort(data1);
+        reader.close();
+        input.close();
+
+
+    }
+    public static void oftenBytes(ArrayList<Integer> data, HashMap<Integer, Integer> data1){
+
+        int value = 0;
+        for (int i = 0; i < data.size(); i++)
+        {
+            if (data1.containsKey(data.get(i))){
+                data1.put(data.get(i),value++);
+            }
+            else {
+                data1.put(data.get(i), value );
+            }
+        }
+    }
+
+    public static void sort(HashMap<Integer, Integer> data){
+        ArrayList sortedList = new ArrayList(data.entrySet());
+        Collections.sort(sortedList, new Comparator<Map.Entry<Integer, Integer>>()
+        {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2)
+            {
+                return e1.getValue().compareTo(e2.getValue());
+            }
+        });
+
+            System.out.println(sortedList.get(sortedList.size()-1));
+    }
     }
 }
