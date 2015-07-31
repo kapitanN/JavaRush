1ecf8037dbbf35cedc22256d07b7afcecb63bfd5
nikita <nikita.shapovalov.94@gmail.com>
2015-08-01 00:16:35 +0300

    from linux

--- src/com/javarush/test/level18/lesson05/task02/Solution.java
+++ src/com/javarush/test/level18/lesson05/task02/Solution.java
@@ -8,7 +8,20 @@ package com.javarush.test.level18.lesson05.task02;
 Подсказка: нужно сравнивать с ascii-кодом символа ','
 */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        FileInputStream input = new FileInputStream(name);
+        int count = 0;
+        while (input.available()>0){
+            //if (input.read() == ",")
+        }
     }
 }
