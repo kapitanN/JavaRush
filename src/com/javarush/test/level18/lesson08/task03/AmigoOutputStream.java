e9d174c7b908fda8389c02aea1c051c3615a897e
nikita <nikita.shapovalov.94@gmail.com>
2015-08-06 22:55:33 +0300

    from linux

--- src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
+++ src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
@@ -1,8 +1,7 @@
 package com.javarush.test.level18.lesson08.task03;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.FileChannel;
 
 /* AmigoOutputStream
 1 Измените класс AmigoOutputStream так, чтобы он стал Wrapper-ом для класса FileOutputStream
@@ -12,12 +11,47 @@ import java.io.IOException;
 2.3 закрыть поток методом close()
 */
 
-public class AmigoOutputStream {
-    public static String fileName = "C:/tmp/result.txt";
+public class AmigoOutputStream extends FileOutputStream {
+    public static String fileName = "home/nikita/data.txt";
+    private FileOutputStream out;
 
+    public AmigoOutputStream(FileOutputStream out) throws FileNotFoundException
+    {
+        super(fileName);
+        this.out = out;
+    }
+    @Override
+    public void write(int b) throws IOException
+    {
+        out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException
+    {
+        out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException
+    {
+        out.write(b, off, len);
+    }
+
+    @Override
+    protected void finalize() throws IOException
+    {
+        super.finalize();
+    }
+
+    public void close() throws IOException
+    {
+        out.flush();
+        out.write("JavaRush © 2012-2013 All rights reserved.".getBytes());
+        out.close();
+    }
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
 
-}
 }
