657bb757cbafa0cd3a667a9ac39f276e1880f114
Nikita Shapovalov <nikita.shapovalov.94@gmail.com>
2015-09-14 21:49:17 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/com/javarush/test/level18/lesson10/home01/Solution.java

--- src/com/javarush/test/level18/lesson10/home01/Solution.java
+++ src/com/javarush/test/level18/lesson10/home01/Solution.java
@@ -29,7 +29,7 @@ public class Solution
 
 
 
-            in.close();
+           // in.close();
         }
 
 
--- src/com/javarush/test/level18/lesson10/home03/Solution.java
+++ src/com/javarush/test/level18/lesson10/home03/Solution.java
@@ -7,8 +7,29 @@ package com.javarush.test.level18.lesson10.home03;
 Темповые файлы создавать нельзя, т.к. на сервере заблокирована возможность создания каких любо файлов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileOutputStream out = new FileOutputStream(new File(reader.readLine()),true);
+        FileInputStream in = new FileInputStream(new File(reader.readLine()));
+        FileInputStream in1 = new FileInputStream(new File(reader.readLine()));
+         while (in.available()>0){
+             byte[] arr = new byte[in.available()];
+             int count = in.read(arr);
+             out.write(arr, 0, count);
+         }
+
+        while (in1.available()>0){
+            byte[] arr = new byte[in1.available()];
+            int count = in1.read(arr);
+            out.write(arr, 0, count);
+        }
+
+        reader.close();
+        out.close();
+        in.close();
+        in1.close();
     }
 }
