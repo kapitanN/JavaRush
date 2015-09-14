95baaabe20cb51bbb893feb74218ed8f1c4c4cdf
Nikita Shapovalov <nikita.shapovalov.94@gmail.com>
2015-09-15 00:28:16 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/com/javarush/test/level18/lesson10/home01/Solution.java

--- src/com/javarush/test/level18/lesson10/home04/Solution.java
+++ src/com/javarush/test/level18/lesson10/home04/Solution.java
@@ -7,8 +7,28 @@ package com.javarush.test.level18.lesson10.home04;
 Темповые файлы создавать нельзя, т.к. на сервере заблокирована возможность создания каких любо файлов
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //FileOutputStream out = new FileOutputStream(reader.readLine());
+        //FileInputStream in = new FileInputStream(reader.readLine());
+        /*while (in.available()>0){
+            byte[] arr = new byte[in.available()];
+            int count = in.read(arr);
+            out.write(arr,0,count);
+        }
+
+        reader.close();
+        out.close();
+        in.close();*/
+        RandomAccessFile data = new RandomAccessFile(reader.readLine(), " rw");
+        RandomAccessFile data1 = new RandomAccessFile(reader.readLine(), " rw");
+        data.seek(0);
+        String s = data.readLine();
+        System.out.println(s);
+
 
     }
 }
