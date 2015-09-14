1c1c985559d0c7493171affbf1dd49ca08154e46
Nikita Shapovalov <nikita.shapovalov.94@gmail.com>
2015-09-15 00:48:27 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/com/javarush/test/level18/lesson10/home01/Solution.java

--- src/com/javarush/test/level18/lesson10/home04/Solution.java
+++ src/com/javarush/test/level18/lesson10/home04/Solution.java
@@ -12,22 +12,51 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        //FileOutputStream out = new FileOutputStream(reader.readLine());
-        //FileInputStream in = new FileInputStream(reader.readLine());
-        /*while (in.available()>0){
-            byte[] arr = new byte[in.available()];
-            int count = in.read(arr);
-            out.write(arr,0,count);
+        File file1 = new File(reader.readLine());
+        File file2 = new File(reader.readLine());
+
+        FileInputStream in1 = new FileInputStream(file1);
+
+        FileInputStream in2 = new FileInputStream(file2);
+
+        byte[] data2 = new byte[0];
+        byte[] data1 = new byte[0];
+
+
+        while(in2.available() > 0){
+            data2 = new byte[in2.available()];
+            int count = in2.read(data2);
         }
 
+        while(in1.available() > 0){
+            data1 = new byte[in1.available()];
+            int count = in1.read(data1);
+        }
+
+        in1.close();
+        FileOutputStream out1 = new FileOutputStream(file1);
+
+        byte[] res = new byte[data1.length + data2.length];
+
+        System.arraycopy(data2, 0, res, 0, data2.length);
+        System.arraycopy(data1, 0, res, data2.length, data1.length);
+
+        out1.write(res);
+
+
         reader.close();
-        out.close();
-        in.close();*/
-        RandomAccessFile data = new RandomAccessFile(reader.readLine(), " rw");
-        RandomAccessFile data1 = new RandomAccessFile(reader.readLine(), " rw");
-        data.seek(0);
-        String s = data.readLine();
+
+        in2.close();
+        out1.close();
+        /*File name1 = new File(reader.readLine());
+        File name2 = new File(reader.readLine());
+        RandomAccessFile data = new RandomAccessFile(name1, "rw");
+        RandomAccessFile data1 = new RandomAccessFile(name2, "rw");
+        String s = data1.readLine();
         System.out.println(s);
+        data.seek(0);
+        data.writeBytes(s);*/
+
 
 
     }
