d2a27956d9aa5da646265e87ea9904a0d4a53fa7
nikita <nikita.shapovalov.94@gmail.com>
2015-09-19 23:49:40 +0300

    from linux

--- src/com/javarush/test/level18/lesson10/home05/Solution.java
+++ src/com/javarush/test/level18/lesson10/home05/Solution.java
@@ -12,13 +12,46 @@ package com.javarush.test.level18.lesson10.home05;
 */
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws IOException{
 
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        FileInputStream in = new FileInputStream(reader.readLine());
-        FileOutputStream out = new FileOutputStream(reader.readLine());
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+        reader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName1));
+        String str;
+        String[] strMass;
+        double tmp;
+
+        while ((str = fileReader.readLine()) != null) {
+            strMass = str.split(" ");
+            for (String strMas : strMass)
+            {
+                tmp = Math.round(Double.parseDouble(strMas));
+                numbers.add((int)tmp);
+            }
+        }
+        fileReader.close();
+
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileName2));
+        String num = "";
+        for (int a : numbers)
+            num = num+a+" ";
+        fileWriter.write(num);
+        fileWriter.close();
+        /*
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        File file1 = new File(reader.readLine());
+        File file2 = new File(reader.readLine());
+
+        FileInputStream in = new FileInputStream(file1);
+        FileOutputStream out = new FileOutputStream(file2);
 
         while (in.available()>0){
             byte [] data = new  byte[in.available()];
@@ -35,18 +68,18 @@ public class Solution {
         StringBuilder builder = new StringBuilder();
         byte [] res;
         String [] str = new String(data).split(" ");
-        double [] tmp = new double[str.length];
+        long [] tmp = new long[str.length];
         for (int i = 0; i < str.length; i++)
         {
             tmp[i] =  Math.round(Double.valueOf(str[i]));
         }
 
-        for (double i : tmp){
+        for (long i : tmp){
             builder.append(i);
             builder.append(" ");
         }
 
         res = builder.toString().getBytes();
-        return res;
+        return res;*/
     }
 }
