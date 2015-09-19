a0129762b37c551fd3b842420a07ca7427998f66
nikita <nikita.shapovalov.94@gmail.com>
2015-09-19 23:44:33 +0300

    from linux

--- src/com/javarush/test/level18/lesson10/home05/Solution.java
+++ src/com/javarush/test/level18/lesson10/home05/Solution.java
@@ -11,8 +11,42 @@ package com.javarush.test.level18.lesson10.home05;
 3.51 - 4
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream in = new FileInputStream(reader.readLine());
+        FileOutputStream out = new FileOutputStream(reader.readLine());
+
+        while (in.available()>0){
+            byte [] data = new  byte[in.available()];
+            in.read(data);
+            out.write(getData(data));
+
+        }
+
+        in.close();
+        out.close();
+    }
+
+    private static byte [] getData(byte[] data){
+        StringBuilder builder = new StringBuilder();
+        byte [] res;
+        String [] str = new String(data).split(" ");
+        double [] tmp = new double[str.length];
+        for (int i = 0; i < str.length; i++)
+        {
+            tmp[i] =  Math.round(Double.valueOf(str[i]));
+        }
+
+        for (double i : tmp){
+            builder.append(i);
+            builder.append(" ");
+        }
+
+        res = builder.toString().getBytes();
+        return res;
     }
 }
