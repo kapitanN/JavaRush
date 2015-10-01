eb1c40a73aa3e98cee1bc42d712f8cf516542e01
nikita <nikita.shapovalov.94@gmail.com>
2015-10-01 20:05:31 +0300

    from linux

--- src/com/javarush/test/level19/lesson03/task02/AdapterFileOutputStream.java
+++ src/com/javarush/test/level19/lesson03/task02/AdapterFileOutputStream.java
@@ -4,7 +4,14 @@ package com.javarush.test.level19.lesson03.task02;
 Используйте класс AdapterFileOutputStream, чтобы адаптировать FileOutputStream к новому интерфейсу AmigoStringWriter
 */
 
+import java.io.FileOutputStream;
+
 public class AdapterFileOutputStream {
+    private static FileOutputStream fileOutputStream;
+    public AdapterFileOutputStream(FileOutputStream fileOutputStream){
+        this.fileOutputStream = fileOutputStream;
+    }
 
 }
+
 
