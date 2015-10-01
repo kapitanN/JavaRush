13889e8aa56121e5315c200ec840ea20e8126f30
nikita <nikita.shapovalov.94@gmail.com>
2015-10-02 00:03:10 +0300

    from linux

--- src/com/javarush/test/level19/lesson03/task03/Solution.java
+++ src/com/javarush/test/level19/lesson03/task03/Solution.java
@@ -17,64 +17,70 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    public static Map<String, String> countries = new HashMap<String, String>();
+    public static Map<String,String> countries = new HashMap<String,String>();
 
-    public static class IncomeDataAdapter {
-        static{
-            countries.put("UA", "Ukraine");
-            countries.put("RU", "Russian");
-            countries.put("CA", "Canada");
-        }
-        public static IncomeData incomeData;
-        public IncomeDataAdapter(IncomeData incomeData){
-            this.incomeData = incomeData;
-        }
+    static {
+        countries.put("UA","Ukraine");
+        countries.put("RU","Russia");
+        countries.put("CA","Canada");
+    }
 
-        public String getCompanyName(){
-            return incomeData.getCompany();
+    public static class IncomeDataAdapter implements Customer, Contact {
+        private IncomeData data;
+        public  IncomeDataAdapter(IncomeData data){
+            this.data=data;
         }
 
-        public String getCountryName(){
-            return countries.get(incomeData.getCountryCode());
+
+        @Override
+        public String getName()
+        {
+            return data.getContactLastName() + ", " + data.getContactFirstName();
         }
 
-        public String getName(){
-            return incomeData.getContactLastName() + "," + incomeData.getContactFirstName();
+        @Override
+        public String getPhoneNumber()
+        {
+            String m = String.format("+%d(%2$s)%3$s-%4$s-%5$s",data.getCountryPhoneCode(),
+                    String.format("%010d", data.getPhoneNumber()).substring(0, 3),
+                    String.format("%010d", data.getPhoneNumber()).substring(3, 6),
+                    String.format("%010d", data.getPhoneNumber()).substring(6, 8),
+                    String.format("%010d", data.getPhoneNumber()).substring(8));
+
+            return m;
+
         }
 
-        public String getPhoneNumber(){
-            String s = String.format("+%d(%2$s)%3$s-%4$s-%5$s",incomeData.getCountryPhoneCode(),
-                    String.format("%010d",incomeData.getPhoneNumber()).substring(0, 3),
-                    String.format("%010d",incomeData.getPhoneNumber()).substring(3,6),
-                    String.format("%010d",incomeData.getPhoneNumber()).substring(6,8),
-                    String.format("%010d",incomeData.getPhoneNumber()).substring(8));
-            return s;
+        @Override
+        public String getCompanyName()
+        {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName()
+        {
+
+            return countries.get(data.getCountryCode());
         }
     }
 
     public static interface IncomeData {
         String getCountryCode();        //example UA
-
         String getCompany();            //example JavaRush Ltd.
-
         String getContactFirstName();   //example Ivan
-
         String getContactLastName();    //example Ivanov
-
         int getCountryPhoneCode();      //example 38
-
         int getPhoneNumber();           //example 501234567
     }
 
     public static interface Customer {
         String getCompanyName();        //example JavaRush Ltd.
-
         String getCountryName();        //example Ukraine
     }
 
     public static interface Contact {
         String getName();               //example Ivanov, Ivan
-
         String getPhoneNumber();        //example +38(050)123-45-67
     }
 }
