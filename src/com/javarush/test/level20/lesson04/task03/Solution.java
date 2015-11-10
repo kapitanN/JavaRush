ee9443e2c14ff89a120138619e4511b9bbf5300e
Nikita Shapovalov <nikita.shapovalov.94@gmail.com>
2015-11-10 19:26:50 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/com/javarush/test/level18/lesson10/home01/Solution.java

--- src/com/javarush/test/level20/lesson04/task03/Solution.java
+++ src/com/javarush/test/level20/lesson04/task03/Solution.java
@@ -45,6 +45,8 @@ public class Solution implements Serializable {
             return ourInstance;
         }
 
+        protected Object readResolve(){}
+
         private Singleton() {
         }
     }
